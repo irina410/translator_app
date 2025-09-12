3cb7d3f8ffa99e5882afa85623ca889245f81d1e
Irina <Irisha.Kalagova@yandex.ru>
2025-09-12 11:30:59 +0300

    add RepositoryModule

---
+++ app/src/main/java/com/example/translatorapp/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.example.translatorapp.di
+
+import com.example.translatorapp.domain.repository.WordRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWordRepository(
+        impl: WordRepositoryImpl
+    ): WordRepository
+}
