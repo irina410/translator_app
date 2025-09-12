815fabaf9efa9839a36abaf8d34ed824cc37ca68
Irina <Irisha.Kalagova@yandex.ru>
2025-09-12 21:25:48 +0300

    add event for delete

--- app/src/main/java/com/example/translatorapp/ui/translator_screen/TranslatorEvent.kt
+++ app/src/main/java/com/example/translatorapp/ui/translator_screen/TranslatorEvent.kt
@@ -4,4 +4,5 @@ sealed class TranslatorEvent {
     data class EnteredWord(val word: String) : TranslatorEvent()
     object TranslateClicked : TranslatorEvent()
     data class ToggleFavorite(val wordId: String) : TranslatorEvent()
+    data class DeleteWord(val wordId: String) : TranslatorEvent()
 }
